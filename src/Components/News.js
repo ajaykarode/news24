669ac5e4c73b8df9cc487abf77fec7084390fad4
ajay <ajay.karode1212@gmail.com>
2023-04-26 21:12:48 +0530

    edited

--- src/Components/News.js
+++ src/Components/News.js
@@ -33,14 +33,6 @@ const News = (props) => {
     updateNews();
   }, []);
 
-  // const handlePreviousClick = async () => {
-  //   setPage(page - 1)
-  //   updateNews(); 
-
-  // const handleNextClick = async () => {
-  //   setPage(page + 1)
-  //   updateNews();
-  // };
 
   const fetchMoreData = async () => {
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
